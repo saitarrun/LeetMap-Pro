c3a10daa97a62241970d8445621137e76ef5056c
saitarrun <saitarrunpitta@gmail.com>
2026-09-12 14:34:24 -0500

    Simplify robots.txt to standard single rule block

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,46 +5,18 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: ['/', '/_next/static/', '/__clerk/npm/'],
-        disallow: ['/api/', '/__clerk/v1/'],
-      },
-      {
-        userAgent: 'Googlebot',
-        allow: ['/', '/_next/static/', '/__clerk/npm/'],
-        disallow: ['/api/', '/__clerk/v1/'],
-      },
-      {
-        userAgent: 'Google-Extended',
-        allow: ['/', '/_next/static/', '/__clerk/npm/'],
-        disallow: ['/api/', '/__clerk/v1/'],
-      },
-      {
-        userAgent: 'Googlebot-Image',
         allow: '/',
-      },
-      {
-        userAgent: 'Bingbot',
-        allow: ['/', '/_next/static/', '/__clerk/npm/'],
-        disallow: ['/api/', '/__clerk/v1/'],
-      },
-      {
-        userAgent: 'bingbot',
-        allow: ['/', '/_next/static/', '/__clerk/npm/'],
-        disallow: ['/api/', '/__clerk/v1/'],
-      },
-      {
-        userAgent: 'msnbot',
-        allow: ['/', '/_next/static/', '/__clerk/npm/'],
-        disallow: ['/api/', '/__clerk/v1/'],
-      },
-      {
-        userAgent: 'Applebot',
-        allow: ['/', '/_next/static/', '/__clerk/npm/'],
-        disallow: ['/api/', '/__clerk/v1/'],
+        disallow: [
+          '/api/',
+          '/account',
+          '/settings',
+          '/sign-in',
+          '/sign-up',
+        ],
       },
     ],
     sitemap: 'https://leetmap-pro.vercel.app/sitemap.xml',
-    host: 'leetmap-pro.vercel.app',
   };
 }
+
 
